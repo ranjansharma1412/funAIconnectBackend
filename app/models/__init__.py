515e4587ba0e7476e85d6f60eeacffc2e0dfea2a
Ranjan Sharma <ranjansharma1412@gmail.com>
2026-03-03 11:34:54 +0530

    Fixed story liked

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,4 @@ from app.models.comment import Comment
 from app.models.like import Like
 from app.models.friend import Friend
 from app.models.story import Story
+from app.models.story_like import StoryLike
