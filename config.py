090d5237300c83fe66e3d147a068a9666141e3df
Ranjan Sharma <ranjansharma1412@gmail.com>
2026-03-03 10:33:53 +0530

    Replaced databse with postgre sql

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-dev-key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///dev.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres@localhost:5432/funai_dev'
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app/static/uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max limit
 
